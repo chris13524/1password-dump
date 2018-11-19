81607a295324ce06bcf084da333cf3f3c231778f
chris13524 <chris@hichris.com>
2018-11-19 18:30:31 -0500

    Added comments and updated README.

--- 1password-dump.sh
+++ 1password-dump.sh
@@ -2,28 +2,40 @@
 
 set -e
 
+# Create the out directory.
 outDir=$1
 if [ -z "$outDir" ]; then
   echo "Missing required parameter: output directory"
   exit 1
 fi
-
 mkdir -p "$outDir"
 
+# Iterate over each vault.
 for vault in `op list vaults | jq ".[]" -c`; do
+  # Obtain some information about the vault.
   vaultId=`echo $vault | jq ".uuid" -r`
   vaultName=`echo $vault | jq ".name" -r`
+
+  # Figure where to dump the vault to.
   vaultDir="$outDir/$(echo $vaultName | sed 's/\//\\\//g')"
   if [ -f "$vaultDir" ]; then
+    # If the vault already exists, append its ID to the end to prevent overwriting.
     vaultDir="$vaultDir.$vaultId"
   fi
+
+  # Make a folder for the vault.
   echo "Dumping vault $vaultName to $vaultDir"
   mkdir -p "$vaultDir"
+
+  # Iterate over all items (including trash) inside the vault.
   for itemId in `op list items --include-trash | jq ".[].uuid" -cr`; do
+    # Obtain some information about the item.
     item=`op get item $itemId --include-trash`
     itemTitle=`echo $item | jq ".overview.title" -r`
     trashed=`echo $item | jq ".trashed" -r`
     itemBody=`echo $item | jq "."` # just to format it
+
+    # Figure where to dump the item to.
     dot=""
     trashedText=""
     if [ "$trashed" == "Y" ]; then
@@ -32,8 +44,11 @@ for vault in `op list vaults | jq ".[]" -c`; do
     fi
     itemFile="$vaultDir/$dot$(echo $itemTitle | sed 's/\//\\\//g')"
     if [ -f "$itemFile" ]; then
+      # If the item file already exists, append its ID to the end to prevent overwriting.
       itemFile="$itemFile.$itemId"
     fi
+
+    # Write the item to a file.
     echo "  Dumping item $itemTitle$trashedText to $itemFile"
     echo "$itemBody" > "$itemFile"
   done
--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 This is a simple script that will dump all data from a 1Password account into a directory of your choosing.
 
+If you're like me, you don't want your entire set of passwords and other important information being stored in a single location. You can use this script to dump all of this data (possibly on a regular basis) in the event something happens to 1Password.
+
 ## Usage
 
 Login to 1Password as usual using `eval $(op signin)`.
 
 Dump your data: `./1password-dump.sh <dump dir>`
+
+## Dependencies
+
+You'll need to have the `jq` utility installed to parse JSON.
